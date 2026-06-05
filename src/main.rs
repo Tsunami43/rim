d1e40c4cfa541b3e9031552a73e8fd97160c9121
Tsunami43 <n3m0_22@mail.ru>
2026-06-05 17:43:26 +0300

    feat: add RawModeGuard and Editor

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,47 @@ use crossterm::{
     event::{read, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-fn main() -> Result<()> {
-    enable_raw_mode()?;
-    loop {
-        let event = read()?;
-        let key = event.as_key_event().unwrap();
-        println!("Press button(code): {:?}", key.code);
-        if key.code == KeyCode::Esc {
-            break;
+
+struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { should_quit: false }
+    }
+    fn run(&mut self) -> Result<()> {
+        loop {
+            let event = read()?;
+            let key = event.as_key_event().unwrap();
+            println!("Press button(code): {:?}", key.code);
+            if key.code == KeyCode::Esc {
+                self.should_quit = true;
+            }
+            if self.should_quit {
+                break Ok(());
+            }
         }
     }
-    disable_raw_mode()?;
-    Ok(())
+}
+
+struct RawModeGuard;
+
+impl RawModeGuard {
+    fn new() -> Result<Self> {
+        enable_raw_mode()?;
+        Ok(Self)
+    }
+}
+
+impl Drop for RawModeGuard {
+    fn drop(&mut self) {
+        let _ = disable_raw_mode();
+    }
+}
+
+fn main() -> Result<()> {
+    let _guard = RawModeGuard::new()?;
+    let mut editor = Editor::new();
+    editor.run()
 }
