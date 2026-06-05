ae16d83b74433a850d5a95c01a3da534c0b1d340
Tsunami43 <n3m0_22@mail.ru>
2026-06-05 17:02:14 +0300

    feat: add read press keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{self, io::Result};
+
+use crossterm::{
+    event::{read, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+fn main() -> Result<()> {
+    enable_raw_mode()?;
+    loop {
+        let event = read()?;
+        let key = event.as_key_event().unwrap();
+        println!("Press button(code): {:?}", key.code);
+        if key.code == KeyCode::Esc {
+            break;
+        }
+    }
+    disable_raw_mode()?;
+    Ok(())
 }
