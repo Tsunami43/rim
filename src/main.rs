048eea4a4c31ecc40c544b4a7827910aa74904dd
Tsunami43 <n3m0_22@mail.ru>
2026-06-06 00:41:00 +0300

    feat: add keybindings

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ enum Mode {
 
 struct Editor {
     should_quit: bool,
+    awaiting_g: bool,
     document: Document,
     mode: Mode,
     position_x: u16,
@@ -35,6 +36,7 @@ impl Editor {
         };
         Self {
             should_quit: false,
+            awaiting_g: false,
             document,
             mode: Mode::Normal,
             position_x: 0,
@@ -81,35 +83,82 @@ impl Editor {
     }
 
     fn handler_normal(&mut self, key: KeyEvent) {
+        let was_awaiting_g = self.awaiting_g;
+        self.awaiting_g = false;
+
         match key.code {
             // Exit (ctrl+q)
             KeyCode::Char('q') if key.modifiers.contains(KeyModifiers::CONTROL) => {
                 self.should_quit = true
             }
-            // Switch mode to Insert
-            KeyCode::Char('I') => self.mode = Mode::Insert,
-            // Move left
+            // Switch mode to Insert (a)
+            KeyCode::Char('a') => {
+                self.position_x += 1;
+                self.mode = Mode::Insert
+            }
+            // Switch mode to start row Insert (shift + a / A)
+            KeyCode::Char('A') => {
+                self.position_x = self.current_row_len();
+                self.mode = Mode::Insert
+            }
+            // Switch mode to Insert (i)
+            KeyCode::Char('i') => self.mode = Mode::Insert,
+            // Switch mode to start row Insert (shift + i / I)
+            KeyCode::Char('I') => {
+                self.position_x = 0;
+                self.mode = Mode::Insert
+            }
+            // Move left (h)
             KeyCode::Char('h') => self.position_x = self.position_x.saturating_sub(1),
-            // Move down
+            // Move down (j)
             KeyCode::Char('j') => {
                 if (self.position_y as usize) + 1 < self.document.rows.len() {
                     self.position_y = self.position_y.saturating_add(1)
                 }
-                if self.current_row_len() <= self.position_x {
-                    self.position_x = self.current_row_len().saturating_sub(1);
-                }
+                self.clamp_x_to_row();
             }
-            // Move up
+            // Move up (k)
             KeyCode::Char('k') => {
                 self.position_y = self.position_y.saturating_sub(1);
-                if self.current_row_len() <= self.position_x {
-                    self.position_x = self.current_row_len().saturating_sub(1);
-                }
+                self.clamp_x_to_row();
             }
-            // Move right
+            // Move right (l)
             KeyCode::Char('l') if self.current_row_len() > self.position_x.saturating_add(1) => {
                 self.position_x = self.position_x.saturating_add(1)
             }
+            // Move to start document (gg)
+            KeyCode::Char('g') if was_awaiting_g => {
+                self.position_y = 0;
+                self.clamp_x_to_row();
+            }
+            // Move to end document (G)
+            KeyCode::Char('G') => {
+                self.position_y = self.document.rows.len().saturating_sub(1) as u16;
+                self.clamp_x_to_row();
+            }
+            // Move to half page up (ctrl + u)
+            KeyCode::Char('u') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                let (_, rows) = size().unwrap();
+                self.position_y = self.position_y.saturating_sub(rows / 2);
+                self.clamp_x_to_row();
+            }
+            // Move to half page down (ctrl + d)
+            KeyCode::Char('d') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                let (_, rows) = size().unwrap();
+                let move_y = rows / 2;
+                let max_y = self.document.rows.len() as u16;
+
+                let new_y = self.position_y.saturating_add(move_y);
+
+                if new_y >= max_y {
+                    self.position_y = max_y.saturating_sub(1);
+                } else {
+                    self.position_y = new_y;
+                }
+                self.clamp_x_to_row();
+            }
+            // Switch state g
+            KeyCode::Char('g') => self.awaiting_g = true,
             _ => {}
         }
     }
@@ -163,6 +212,13 @@ impl Editor {
             self.offset_x = self.position_x;
         } else if self.position_x >= self.offset_x + cols {
             self.offset_x = self.position_x - cols + 1;
+        }
+    }
+
+    fn clamp_x_to_row(&mut self) {
+        let max_x = self.current_row_len().saturating_sub(1);
+        if self.position_x > max_x {
+            self.position_x = max_x;
         }
     }
 }
